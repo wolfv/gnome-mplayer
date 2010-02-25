7cae45b4c851db0fb50d06786c83437aa8368732
kdekorte <kdekorte@747cd070-b240-0410-9b08-91f62042478d>
2010-02-25 21:13:07 +0000

    tagging 0.9.9.2

---
+++ tags/v0_9_9_2/config.h
@@ -0,0 +1,122 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* defined enable panscan functionality */
+/* #undef ENABLE_PANSCAN */
+
+/* GETTEXT package name */
+#define GETTEXT_PACKAGE "gnome-mplayer"
+
+/* Define to 1 if you have GIO support */
+#define GIO_ENABLED 1
+
+/* Define to 1 if you have GLIB 2.14 or higher */
+#define GLIB2_14_ENABLED 1
+
+/* Define to 1 if you have gpod support */
+#define GPOD_ENABLED 1
+
+/* Define to 1 if you have GTK 2.12 or higher */
+#define GTK2_12_ENABLED 1
+
+/* Define to 1 if you have GTK 2.14 or higher */
+#define GTK2_14_ENABLED 1
+
+/* Define to 1 if you have asoundlib development packages */
+#define HAVE_ASOUNDLIB 1
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
+/* Define to 1 to use GConf to store preferences */
+#define HAVE_GCONF 1
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <mntent.h> header file. */
+#define HAVE_MNTENT_H 1
+
+/* defined if you build the nautilus plugin */
+#define HAVE_NAUTILUS 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have musicbrainz3 support */
+#define MUSICBRAINZ_ENABLED 1
+
+/* Define to 1 if you have NOTIFY support */
+#define NOTIFY_ENABLED 1
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "gnome-mplayer"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Defined if you are using gnome-power-manager 2.26 or higher */
+#define SM_INHIBIT 1
+
+/* Defined if you are using gnome-power-manager below 2.26 */
+#define SS_INHIBIT 0
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.9.9.2"
+
+/* When defined we have direct XScrnSaver control */
+#define XSCRNSAVER_ENABLED 1
